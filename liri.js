5f13641df548557454bc850f773ea0157d656138
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-06-14 19:28:16 -0700

    Concert request functioning

--- liri.js
+++ liri.js
@@ -11,6 +11,33 @@ var pointer = process.argv[3];
 switch(command){
     case "concert-this":
         console.log("you're looking for a concert");
+        var fullQuery = "https://rest.bandsintown.com/artists/" + pointer + "/events?app_id=codingbootcamp"
+        console.log(fullQuery);
+        axios.get(fullQuery).then(
+        function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+            } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+            } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
         break;
     case "spotify-this-song":
         console.log("you're looking for a song");
@@ -25,3 +52,4 @@ switch(command){
         console.log("you've done something WRONG");
 
 }
+
