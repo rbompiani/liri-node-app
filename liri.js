0d0de3671900e568fc5f5ff371368af263ace6ed
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-06-14 19:14:50 -0700

    installed package dependencies and tested first argv parameter

--- liri.js
+++ liri.js
@@ -1,7 +1,27 @@
 require("dotenv").config();
+const axios = require('axios');
+var Spotify = require('node-spotify-api');
+var moment = require('moment');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var pointer = process.argv[3];
 
+switch(command){
+    case "concert-this":
+        console.log("you're looking for a concert");
+        break;
+    case "spotify-this-song":
+        console.log("you're looking for a song");
+        break;
+    case "movie-this":
+        console.log("you're looking for a movie");
+        break;
+    case "do-what-it-says":
+        console.log("you're bossy");
+        break;
+    default:
+        console.log("you've done something WRONG");
+
+}
