c8be5dd58af1ac764a84e971596742dc85052a66
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-06-16 16:33:21 -0700

    added OMDB search functionality & response formatting

--- liri.js
+++ liri.js
@@ -67,7 +67,42 @@ switch(command){
     
     /* ---- MOVIES ---- */
     case "movie-this":
+
+    var fullQuery="http://www.omdbapi.com/?apikey=trilogy&t="+pointer;
         console.log("you're looking for a movie");
+        axios.get(fullQuery).then(
+            function(response) {
+                //console.log(response);
+                console.log("Title: "+response.data.Title);
+                console.log("Year: "+response.data.Year);
+                console.log("IMDB Rating: "+response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: "+response.data.Ratings[1].Value);
+                console.log("Country: "+response.data.Country);
+                console.log("Language: "+response.data.Language);
+                console.log("Plot: "+response.data.Plot);
+                console.log("Actors: "+response.data.Actors);
+            
+            })
+            .catch(function(error) {
+                if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+                } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+                } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
         break;
 
     /* ---- RUN FROM FILE ---- */
